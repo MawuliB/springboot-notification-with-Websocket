d0df8c8fa82ded9d1685f2aa6383a35928451834
Mawuli Badassou <mawulibadassou5@gmail.com>
2024-06-11 08:43:15 +0000

    updated readme

--- src/main/java/com/websoket/notification/services/ScheduledEvents.java
+++ src/main/java/com/websoket/notification/services/ScheduledEvents.java
@@ -22,11 +22,10 @@ public class ScheduledEvents {
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final NotificationService notificationService;
 
-    @Scheduled(cron = "0 0 0 * * *")
-    public void sendHelloMessage(){
+    @Scheduled(cron = "0 0 0 * * *") // every day at midnight
+    public void sendHelloMessage(){ // send a hello message to all users
         List<Person> people = (List<Person>) personRepository.findAll();
         for (Person person : people) {
-            log.error("Sending message to {}", person.getName());
             NotificationRequest notification = new NotificationRequest();
             notification.setOwnerId(String.valueOf(person.getId()));
             notification.setContent("Hello " + person.getName() + "!");
